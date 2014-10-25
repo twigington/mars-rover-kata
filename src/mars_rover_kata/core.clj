2691bb6f96fad47d0603c616b71da905a0c72a50
Tom Wigington <tom.wigington@gmail.com>
2014-10-25 07:22:43 -0500

    Add forward method and related tests. Not used by rover yet.

--- src/mars_rover_kata/core.clj
+++ src/mars_rover_kata/core.clj
@@ -5,3 +5,24 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn forward
+  "New position based on direction"
+  [{x :x y :y r :r}]
+  (case r
+    :north {:x x :y (inc y) :r r}
+    :south {:x x :y (dec y) :r r}
+    :east {:x (inc x) :y y :r r}
+    :west {:x (dec x) :y y :r r}
+    :else {x :x y :y r :r}))
+
+(defn move-rover
+  "Yarg"
+  ([start-vector]
+   start-vector)
+  ([start-vector movements]
+   (if (empty? movements)
+     start-vector
+     start-vector))
+  ([start-vector movements obstacles]
+   start-vector))
--- test/mars_rover_kata/core_test.clj
+++ test/mars_rover_kata/core_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [mars-rover-kata.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest forward-north
+  (testing "Move forward while facing north"
+    (is (= {:x 0 :y 1 :r :north} (forward {:x 0 :y 0 :r :north})))))
+
+(deftest forward-south
+  (testing "Move forward while facing south"
+    (is (= {:x 1 :y 0 :r :south} (forward {:x 1 :y 1 :r :south})))))
+
+(deftest no-moves
+  (testing "When no moves are provided, return the original position and direction."
+    (is (= {:x 0 :y 0 :r :north} (move-rover {:x 0 :y 0 :r :north} [])))))
+
+(deftest one-forward
+  (testing "Move Forward Once."
+    (is (= {:x 0 :y 1 :r :north} (move-rover {:x 0 :y 0 :r :north} [:forward])))))
